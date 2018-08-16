3a302ca2aeeedf51a1193c95bab875c92ce08904
Adam Rice <development@hashnotadam.com>
2018-08-16 21:20:51 +0200

    Add and remove items from a list

--- list_quickstart/Program.cs
+++ list_quickstart/Program.cs
@@ -5,13 +5,38 @@ namespace list_quickstart
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            var names = SimpleList();
+            AddAndRemoveFromList(names);
+        }
+
+        static List<string> SimpleList()
         {
             var names = new List<string> { "Adam", "Ana", "Felipe" };
+
             foreach (var name in names)
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            return names;
+        }
+
+        static List<string> AddAndRemoveFromList(List<string> names)
+        {
+            Console.WriteLine();
+
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            return names;
         }
     }
 }
